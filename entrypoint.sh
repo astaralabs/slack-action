71e5c96aef4bd294c97031623ab7fd96b2817ab2
Daniel Lombraña González <teleyinex@gmail.com>
2020-01-09 12:09:48 +0100

    fix: remove check.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -eu
 
-if [[ -z "$SLACK_BOT_TOKEN" ]]; then
-  echo "Set the SLACK_BOT_TOKEN secret."
-  exit 1
-fi
-
 RESPONSE="$(curl -X POST \
      -H "Content-type: application/json; charset=utf8" \
      -H "Authorization: Bearer $SLACK_BOT_TOKEN" \
